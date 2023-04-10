4123312d190ff4779dfc4777aa4e4ee8807d61c0
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2023-04-10 09:14:16 -0300

    (0.1)

---
+++ src/sistemaventasant/SistemaVentasAnt.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemaventasant;
+
+import Vista.Sistema;
+
+/**
+ *
+ * @author tagax
+ */
+public class SistemaVentasAnt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Sistema s = new Sistema();
+        s.setVisible(true);
+    }
+    
+}
